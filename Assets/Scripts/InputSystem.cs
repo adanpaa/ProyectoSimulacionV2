630c32ba9339d54e982e1527fd2ee0c6139dc006
Adan Palacios Avelar <avelaradan2810@gmail.com>
2021-06-14 18:06:38 -0500

    Movimiento automatico a vehiculo

---
+++ Assets/Scripts/InputSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public enum ControlMode { Keyboard = 1, Touch = 2 };
+
+public class InputSystem : MonoBehaviour
+{
+	public ControlMode control;
+	public float Accel;
+	public float Steer;
+	public float Brake;
+	public GameObject UI;
+	public void AccelInput(float input) { Accel = input; }
+	public void SteerInput(float input) { Steer = input; }
+	public void BrakeInput(float input) { Brake = input; }
+	CarController Car;
+
+	void Start()
+	{
+		Car = GetComponent<CarController>();
+	}
+	void Update()
+	{
+		if (control == ControlMode.Keyboard)
+		{
+			Accel = Input.GetAxis("Vertical");
+			Steer = Input.GetAxis("Horizontal");
+			Brake = Input.GetAxis("Jump");
+			UI.SetActive(false);
+		}
+		else
+		{
+			UI.SetActive(true);
+		}
+	}
+	void FixedUpdate()
+	{
+		Car.Move(Steer, Accel, Accel, Brake);
+	}
+}
