ab9c035d6f19a85b2e933234602da3def2ac8535
Adan Palacios Avelar <avelaradan2810@gmail.com>
2021-06-14 20:53:03 -0500

    Agregue reiniciar escena despues de 10s

---
+++ Assets/Scripts/RESTART.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class RESTART : MonoBehaviour
+{
+    public float tiempo_start = 0.0f; //Los segundos por los quales comienza i la variable que utilizaremos para que vaya contando segundos.
+    public float tiempo_end = 60.0f;//Segundos que queremos que pasen para que cambie de escena
+                            // Update is called once per frame
+    public float escena;
+
+    void Update()
+    {
+        tiempo_start += Time.deltaTime;//Función para que la variable tiempo_start vaya contando segundos.
+        if (tiempo_start >= tiempo_end) //Si pasan los segundos que hemos puesto antes...
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
