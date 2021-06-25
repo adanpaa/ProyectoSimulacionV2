582c9182922d866cdc8eb8283252ccb6e9d60f2b
Adan Palacios Avelar <avelaradan2810@gmail.com>
2021-06-24 20:40:51 -0500

    Efectos sonido escena 6

---
+++ Assets/Scripts/Crash.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    public AudioSource crash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crash = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "House_07 (1)")
+        {
+            crash.Play();
+        }
+    }
+}
